c67fbb1ce8e58aed3e62be008c4e65120c221c7b
Stephen Charaton <charaton@icloud.com>
2019-03-17 11:46:36 -0700

    Redirect to profiles index after sign in, in case user doesn't have one

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,11 +5,11 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:email])
     if user&.authenticate(params[:password])
-      sign_in(user)
+      sign_in user
       if user.admin? # if the user is an admin redirect to the user show page
         redirect_to users_path, notice: "Welcome back mighty admin!"
       else # they are just a regular use and redirect to the normal homepage
-        redirect_to profile_path(current_user_profile), notice: "Sign in successful!"
+        redirect_to profiles_path, notice: "Sign in successful!"
       end
     else
       flash[:alert] = "Wrong credentials!"
